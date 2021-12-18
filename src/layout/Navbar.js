0a751d6c38e4a53105a91d1ec13ab004eaff29fc
Luiz Yavorski <leesonego@gmail.com>
2021-12-17 21:08:37 -0300

    removing position sticky in component navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="w-full shadow-md sticky top-0 p-2 md:p-4 content-center backdrop-blur-sm bg-white/30 dark:bg-gray-900/80">
+    <nav className="w-full shadow-md  top-0 p-2 md:p-4 content-center backdrop-blur-sm bg-white/30 dark:bg-gray-900/80">
       <h1 className="font-Fredoka-One text-center text-2xl md:text-4xl antialiased font-normal text-red-600 dark:text-red-400">MyMarvel</h1>
     </nav>
   )
